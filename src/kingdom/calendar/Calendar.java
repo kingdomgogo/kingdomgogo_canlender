22791f90cc7c81b3fe065145d86397c05645428f
kingd <kingd@DESKTOP-SPN63EO>
2018-11-05 16:49:50 +0900

    Calendar.exe2

--- src/kingdom/calendar/Calendar.java
+++ src/kingdom/calendar/Calendar.java
@@ -2,12 +2,11 @@ package kingdom.calendar;
 
 public class Calendar {
  public static void main(String[] args) {
-	System.out.println("일 월 화 수 목 금 토");
-	System.out.println("--------------------");
-	System.out.println("1  2  3  4  5  6  7");
-	System.out.println("8  9  10 11 12 13 14");
-	System.out.println("15 16 17 18 19 20 21");
-	System.out.println("22 23 24 25 26 27 28");
-	System.out.println("29 30 31");
+    int a = 1; 
+    String b = "10"; 
+    
+    int Add = Integer.parseInt(b); 
+    
+    System.out.println(a+Add);
  }
 }
