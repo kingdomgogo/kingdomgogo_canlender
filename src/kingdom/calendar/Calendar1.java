dbcf54dc68e0db4f09c19307be377262d8d23cae
kingd <kingd@DESKTOP-SPN63EO>
2018-11-06 15:50:28 +0900

    using for

--- src/kingdom/calendar/Calendar1.java
+++ src/kingdom/calendar/Calendar1.java
@@ -12,11 +12,14 @@ public class Calendar1 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in); 
 		Calendar1 cal = new Calendar1();
+		System.out.println("반복횟수를 입력하세요.");
+		int re = scn.nextInt();
+		for(int i=0; i<re; i++) {
+	
 		System.out.println("원하는 달을 입력하세요.");
 	    int month = scn.nextInt(); 
-	    
 	    System.out.printf("%d월은 %d까지 입니다.\n", month,cal.getMonth(month));
-	    
+		}
 	    scn.close();
 	}
 			
